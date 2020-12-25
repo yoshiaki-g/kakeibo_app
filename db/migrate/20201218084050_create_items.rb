48162250a5f4facb573a92e09d87b529923b52c8
yoshiaki-g <coyote.g.aki@gmail.com>
2020-12-25 17:15:20 +0900

    chartkick導入

--- db/migrate/20201218084050_create_items.rb
+++ db/migrate/20201218084050_create_items.rb
@@ -1,7 +1,9 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-
+      t.date :date, null: false
+      t.integer :value, null: false
+      t.
       t.timestamps
     end
   end
